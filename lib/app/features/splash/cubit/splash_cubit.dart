0d00e09bbc624a6ca3dadc13f4203969ed275ce0
mirza-d3x <mohammedmirsa010@gmail.com>
2024-06-26 17:58:47 +0530

    splash screen completed

--- lib/app/features/splash/cubit/splash_cubit.dart
+++ lib/app/features/splash/cubit/splash_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shop_management_app/main.dart';
 import 'package:shop_management_app/repository/auth_repository.dart';
 
 part 'splash_state.dart';
@@ -8,17 +9,22 @@ part 'splash_state.dart';
 class SplashCubit extends Cubit<SplashState> {
   SplashCubit(this._authRepository)
       : super(const SplashInitial(isLogedIn: false)) {
-    checkUserLogedIn();
+    init();
   }
   final AuthRepository _authRepository;
 
   User? get currentUser => _authRepository.currentUser;
+
+  void init() {
+    checkUserLogedIn();
+  }
 
   void checkUserLogedIn() async {
     // Checking user already login or not
     // if already login it will emit true otherwise false
     await Future.delayed(const Duration(seconds: 3));
     if (currentUser != null) {
+      kUserId = currentUser!.uid;
       emit(const SplashInitial(isLogedIn: true));
     } else {
       emit(const SplashInitial(isLogedIn: false));
